3175e65ec513ed2cbdb2e4d6fc2f1f1fdc6112de
manhak29 <manha.kan29@gmail.com>
2025-01-08 14:25:48 -0600

    initial

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors()); 
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/att_service', {
